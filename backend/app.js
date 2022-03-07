fb5a366bb8a3372464939e84891c21df96052fb4
bov <bov@bv.local>
2022-03-06 21:57:15 -0600

    db connection done

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 //const { Message } = require('@angular/compiler/src/i18n/i18n_ast');
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const Post = require('./models/post');
 
 const app = express(); //returns an express app
 
+mongoose.connect("mongodb+srv://boirs:xgx3BSJAfRrLF7bI@cluster0.buqn3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(() => {
+    console.log('Connection failed!')
+  })
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 
