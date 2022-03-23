b2e5cac292a1abb2f37a576c5aa8b40985d6dda1
bov <bov@bv.local>
2022-03-23 02:49:38 -0500

    ngModel component HTML

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,7 +15,7 @@ export class PostCreateComponent implements OnInit{
   enteredContent = "";
   private mode = "create";
   private postId: string;
-  private post: Post;
+  post: Post;
 
   @Output() postCreated = new EventEmitter();
 
