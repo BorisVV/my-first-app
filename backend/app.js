c2e60ccd93b6d361eb68b81cf988111a5858c1fd
bov <bov@bv.local>
2022-03-27 09:03:07 -0500

    adding the routes dir/file

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
-const Post = require('./models/post');
+const postRequests = require("./routes/posts");
 
 const app = express(); //returns an express app
 
@@ -25,65 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 
-//  Post the list of posts
-app.post("/api/posts", (req, res, next) => {
-  //const post = req.body; //This line was used before the mongodb
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(postCreated => {
-    res.status(201).json({
-      message: "Post added succesfully!",
-      postId: postCreated._id
-    });
-  });
-});
-
-
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id}, post)
-  .then((result) => {
-    //console.log(result); //This displays information in the terminal
-    res.status(200).json({message: "Post updated succesfully!"});
-  });
-});
-
-//Get the posts or fetch
-app.get('/api/posts', (req, res, next) => {
-  Post.find()
-  .then(documents => {
-    //console.log(documents);
-    res.status(200).json({
-      message: "Posts fecthed succesfully!",
-      posts: documents
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      return res.status(200).json(post);
-    } else {
-      return res.status(404).json({message: "Post id not founded"});
-    }
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  //console.log(req.params.id);
-  Post.deleteOne({_id: req.params.id})
-  .then(result => {
-    res.status(200).json({message: "Post Deleted"});
-    //console.log(result);
-  });
-});
+app.use('/api/posts/', postRequests);
 
 //Wire with server.js as listener
 module.exports = app;
