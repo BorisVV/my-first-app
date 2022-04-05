3be0237518c62f27452e6d7e8d2d4b35e66716be
bov <bov@bv.local>
2022-04-05 02:38:40 -0500

    added service and model folders and signup is working without errors

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "../auth.service";
 
 @Component({
 templateUrl: './signup.component.html',
@@ -8,7 +9,12 @@ styleUrls: ['./signup.component.css']
 export class SignUpComponent{
   isLoading = false;
 
-  onSignUp(form: NgForm){
-    
+  constructor(public authService: AuthService) {}
+
+  onSignup(form: NgForm){
+    if (form.invalid) {
+      return;
+    }
+    this.authService.createUser(form.value.email, form.value.password);
   }
 }
