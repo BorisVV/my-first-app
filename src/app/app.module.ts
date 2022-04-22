f65a286d6bff50a50946f267a9b3b21933d8455b
bov <bov@bv.local>
2022-04-22 14:11:16 -0500

    added a file for the mat modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,6 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog';
-
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent} from './app.component';
@@ -27,6 +17,7 @@ import { SignUpComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -41,18 +32,10 @@ import { ErrorComponent } from './error/error.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularMaterialModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatSliderModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatPaginatorModule,
-    MatProgressSpinnerModule,
-    MatDialogModule,
     HttpClientModule
   ],
 
@@ -60,7 +43,7 @@ import { ErrorComponent } from './error/error.component';
     { provide: HTTP_INTERCEPTORS, useClass:AuthInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass:ErrorInterceptor, multi: true }
   ],
-  
+
   bootstrap: [AppComponent],
   entryComponents: [ErrorComponent]
 })
